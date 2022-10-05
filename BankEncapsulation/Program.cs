4a4310e47417258e099f03ae0635235618e1cc16
Abdiel Corro <acorrop@outlook.com>
2022-10-04 22:26:41 -0500

    exercise done

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,17 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+
+            var bankAccount1 = new BankAccount();
+            Console.WriteLine("Please enter the amount to deposit:");
+            
+            var deposited = double.Parse(Console.ReadLine());
+
+            bankAccount1.Deposit(deposited);
+
+            Console.WriteLine($"Your new balance is: ${bankAccount1.GetBalance()}");
+
+
         }
     }
 }
